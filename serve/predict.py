4690172961f469c49dc93069f2994c61f95c1d61
Fahad Al-Harbi <fahad-eid@outlook.sa>
2021-09-06 10:09:04 +0300

    Process input_data so that it is ready to be sent to our model

--- serve/predict.py
+++ serve/predict.py
@@ -69,9 +69,9 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
+    words = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, words)
 
-    data_X = None
-    data_len = None
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
